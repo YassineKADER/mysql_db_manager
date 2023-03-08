2b969c3d59518e4fb6041e9a005ae74ea45c06a8
EROS <yassine.kader2017fc@gmail.com>
2023-03-08 22:36:55 +0100

    update

---
+++ db_manager/src/db_manager/Database.java
@@ -0,0 +1,11 @@
+package db_manager;
+
+public class Database {
+	private String adress;
+	private String password;
+	Database(String adress, String password){
+		this.adress = adress;
+		this.password = adress;
+	}
+	
+}
